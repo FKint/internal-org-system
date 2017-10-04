d339a6ceb035452d325d5ff295f23ca93370b8df
Floris Kint <floris.kint@gmail.com>
2017-10-04 19:19:29 +0200

    Add web server settings

--- web/server.py
+++ web/server.py
@@ -4,6 +4,11 @@ app = Flask(__name__)
 app.config.from_envvar('WEB_SETTINGS')
 
 
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
+
 @app.route('/')
 def view_home():
     return "Home page"
